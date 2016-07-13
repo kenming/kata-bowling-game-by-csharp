88bee5bbc45de893b13efcf0357969a98b34e396
Kenming Wang <kenming.wang@gmail.com>
2016-07-13 19:59:19 +0800

    Build Code Skeleton for Game & GameTest Class.

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kata.game
+{
+    public class Game
+    {       
+        public void roll(int pins)      // 投球
+        {
+        }
+        
+        public int score()              // 計分
+        {
+            return -1;
+        }
+    }
+}
---
+++ BowlingGameTest/GameTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kata.game;
+
+namespace kata.game.test
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
